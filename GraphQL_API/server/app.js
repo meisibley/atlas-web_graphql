4e14d475d17c25bd3db8904cf432f22886b3df92
meisibley <mkong99@yahoo.com>
2024-07-01 09:58:30 -0500

    task0 - GraphQL Schema

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,4 +1,3 @@
-const { schema } = require('./schema/schema')
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var mongoose = require('mongoose');
@@ -20,7 +19,6 @@ mongoose.connection.once('open', () => {console.log('connected to database');}
 );
 
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
   graphiql: true,
 }));
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
